04186bf16645c205a485233703be9c8a7d4c9286
Mijail Todorovich <mijailtodorovich+git@gmail.com>
2019-05-18 22:31:34 -0300

    Change @progress to @logmsg

--- plotting.jl
+++ plotting.jl
@@ -17,12 +17,14 @@ function render(
 
     scene = plot_lindo(sol(0.0), limits = limits)
     record(scene, path, framerate=fps) do io
-        @progress name="Rendering..." for t = 0.0:1.0/fps:tmax
+        for t = 0.0:1.0/fps:tmax
             u = sol(t)
             push!(scene.plots[end].input_args[1], reshape(u[3,:,:], size(u,2)*size(u,3)))
             push!(scene.plots[end].input_args[2], reshape(u[4,:,:], size(u,2)*size(u,3)))
             recordframe!(io)
+            Base.CoreLogging.@logmsg(-1,"Rendering...",_id = :Render, message="$(t)s", progress=t/tmax)
         end
     end
+    Base.CoreLogging.@logmsg(-1,"Rendering...",_id = :Render, progress="done")
     @info "Saved animation to $path"
 end
