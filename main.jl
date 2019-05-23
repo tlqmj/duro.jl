64ba3739e457b515fbd5f0b8252cc974737b7a4b
Mijail Todorovich <mijailtodorovich+git@gmail.com>
2019-05-23 13:00:23 -0300

    Fix initial conditions

--- main.jl
+++ main.jl
@@ -1,5 +1,5 @@
 @info "Starting..."
-using DifferentialEquations, Makie
+using LinearAlgebra, StaticArrays, DifferentialEquations, Makie, ODEInterfaceDiffEq
 using BSON: @save, @load
 include("system.jl")
 include("util.jl")
@@ -10,9 +10,11 @@ N = M = 3;
 p = (k_m=0.2, l₀ = 1.0)
 t = (0.0, 60.0)
 
-u₀ = uniform_grid(N, M, p[:l₀])
-u₀[3,:,1] .+= 0.3*p[:l₀]
-#u₀[4,1,1] += -0.25*p[:l₀]
+u₀ = uniform_grid(M, N, p[:l₀])
+for i in 1:N
+    u₀[3,i,1] += 0.3*p[:l₀]
+    u₀[3,i,M] -= 0.3*p[:l₀]
+end
 
 @info "Displaying initial positions."
 display(plot_lindo(u₀))
